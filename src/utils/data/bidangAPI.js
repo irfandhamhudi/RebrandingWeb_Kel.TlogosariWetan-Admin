1ad6bf0dd3f2f1a8a7539475a019557675b148af
irfandhamhudi <irfandhamhudi712@gmail.com>
2025-07-12 23:58:08 +0700

    fixedd minor

--- src/utils/data/bidangAPI.js
+++ src/utils/data/bidangAPI.js
@@ -20,7 +20,7 @@ export const createBidang = async (formData) => {
 export const getAllBidang = async () => {
   try {
     const response = await axios.get(`${API_URL}/all`);
-    return response.data;
+    return response.data.data;
   } catch (error) {
     throw error.response?.data || error.message;
   }
